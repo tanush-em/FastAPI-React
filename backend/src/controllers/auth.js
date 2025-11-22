98b9fef3b7b58b2b93fb58144961afdbbca20a84
Tanush T M <tanushtm.work@gmail.com>
2025-11-22 22:58:23 +0530

    DB connection added

---
+++ backend/src/controllers/auth.js
@@ -0,0 +1,11 @@
+export async function signup(req, res){
+    res.send("Signup Endpoint");
+}
+
+export async function login(req, res){
+    res.send("Login Endpoint");
+} 
+
+export async function logout(req, res){
+    res.send("Logout Endpoint");
+}
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 
-import router from "./routes/auth.js";
 
+import router from "./routes/auth.js";
+import { connectDB } from "./lib/db.js";
 dotenv.config();
 
 const app = express();
@@ -12,4 +13,5 @@ app.use("/api/auth", router);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+    connectDB();
 })
